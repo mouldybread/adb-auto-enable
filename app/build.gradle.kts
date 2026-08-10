8f933026c776a9cfa88f18677eef341544242d78
mouldybread <38598858+mouldybread@users.noreply.github.com>
2026-08-10 11:16:29 +0800

    chore: bump version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.tpn.adbautoenable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.2.8"
+        versionCode = 9
+        versionName = "0.3"
     }
 
     signingConfigs {
