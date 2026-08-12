f809843f5de4c512bb2650bfd3ce3bb98cd04541
mouldybread <38598858+mouldybread@users.noreply.github.com>
2026-08-12 21:09:18 +0800

    fix: resolve boot broadcast and manual launch bugs (#1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.tpn.adbautoenable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.3.1"
+        versionCode = 11
+        versionName = "0.3.2"
     }
 
     signingConfigs {
